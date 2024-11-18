78303406573bca83b86d39572df39d463d9bc664
Melissa Elliston-Boyes <m.ellistonboyes@gmail.com>
2024-11-18 14:34:47 +1300

    Completed app with refactor

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Melissa Elliston-Boyes on 15/11/2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData: Codable {
+    let rate: Double
+}
---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,17 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Melissa Elliston-Boyes on 15/11/2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+//import Foundation
+//
+//struct CoinModel {
+//    
+//    
+//    var convertedRate: String {
+//        return String(format: "%.2f", rate)
+//    }
+//}
